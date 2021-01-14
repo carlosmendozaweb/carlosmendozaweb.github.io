644723e3ad7b6d330230fa2572ad99aa69bb7105
Carlos M <carlos_matador350@hotmail.com>
2021-01-13 23:55:49 -0600

    Cambios 13/01/21
    - Nuevo portafolio

--- JS/functions.js
+++
@@ -1,7 +0,0 @@
-function Copyright(){
-    var Fecha = new Date();
-    var ParrafoDeFecha = document.getElementById('Copyright');
-
-    ParrafoDeFecha.innerHTML = "©" + Fecha.getFullYear() + " Carlos Mendoza Web";
-}
-window.addEventListener('DOMContentLoaded', Copyright, true);
